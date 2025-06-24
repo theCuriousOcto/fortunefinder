b48d88328bf9213ed5238ae27b5cfe1b461fb998
Micheal Baxter <mtbaxter8@gmail.com>
2025-06-24 00:10:40 -0400

    Added map template JSON, MapTemplate class, index.html layout

--- scripts/classes/map-templates.js
+++ scripts/classes/map-templates.js
@@ -1,3 +1,33 @@
 export default class MapTemplates {
-    
+    #maps = new Map();
+
+    async init() {
+        const response = await fetch(".assets/json/map-templates.json");
+        const data = response.ok ? response.json() : [];
+
+        const results = new Map();
+        for (const row of data) {
+            const key = row.shift();
+            results.set(key, row);
+        }
+
+        this.#maps = results;
+        return this;
+    }
+
+    getMaps() {
+        if (!this.#maps) {
+            console.log("MapTemplates attempted to return maps, but maps weren't initialized.");
+            return;
+        }
+        return this.#maps;
+    }
+
+    getMap(key) {
+        if (!this.#maps) {
+            console.log("MapTemplates attempted to return maps, but maps weren't initialized.");
+            return;
+        }
+        return this.#maps.get(key);
+    }
 }
